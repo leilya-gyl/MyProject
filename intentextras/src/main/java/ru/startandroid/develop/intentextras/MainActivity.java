6c461c8485e5c81b0bca60365778426c8c6a4d46
leil25 <nuriaxmetova93@mail.ru>
2021-04-15 11:13:41 +0300

    Realize method "onClick"
    The method send data from current Activity to ViewActivity.

--- intentextras/src/main/java/ru/startandroid/develop/intentextras/MainActivity.java
+++ intentextras/src/main/java/ru/startandroid/develop/intentextras/MainActivity.java
@@ -2,13 +2,31 @@ package ru.startandroid.develop.intentextras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    EditText etFName, etLName;
+    Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etFName = findViewById(R.id.etFName);
+        etLName = findViewById(R.id.etLName);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ViewActivity.class);
+        intent.putExtra("fname", etFName.getText().toString());
+        intent.putExtra("lname", etLName.getText().toString());
+        startActivity(intent);
     }
 }
