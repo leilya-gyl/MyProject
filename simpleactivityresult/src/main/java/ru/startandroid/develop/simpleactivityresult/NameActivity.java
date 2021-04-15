0d5c88289d3b1037a3ba16a7a3e4d7f16ad6e092
leil25 <nuriaxmetova93@mail.ru>
2021-04-15 13:17:00 +0300

    Create activity named NameActivity
    We create NameActivity, which will send result to main activity.
    User enter text in EditText
    and after clicking on button, return to the main activity.

---
+++ simpleactivityresult/src/main/java/ru/startandroid/develop/simpleactivityresult/NameActivity.java
@@ -0,0 +1,14 @@
+package ru.startandroid.develop.simpleactivityresult;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.name);
+    }
+}
