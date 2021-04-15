45b7d51a310727a5e6f8a766fc5157e029392986
leil25 <nuriaxmetova93@mail.ru>
2021-04-15 11:32:01 +0300

    Realize method "onCreate" in ViewActivity.java
    Use Intent class for getting data from MainActivity
    and showing them in screen.

--- intentextras/src/main/java/ru/startandroid/develop/intentextras/ViewActivity.java
+++ intentextras/src/main/java/ru/startandroid/develop/intentextras/ViewActivity.java
@@ -2,13 +2,26 @@ package ru.startandroid.develop.intentextras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ViewActivity extends AppCompatActivity {
+
+    TextView tvView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+
+        tvView = findViewById(R.id.tvView);
+
+        Intent intent = getIntent();
+
+        String fname = intent.getStringExtra("fname");
+        String lname = intent.getStringExtra("lname");
+
+        tvView.setText("Your name is: " + fname + " " + lname);
     }
 }
