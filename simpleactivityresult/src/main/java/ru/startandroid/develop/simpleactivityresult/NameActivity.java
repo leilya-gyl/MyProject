f8eee2e4e28beaab69e1b5790b88fccc36ed2440
leil25 <nuriaxmetova93@mail.ru>
2021-04-15 13:27:05 +0300

    Fill up "NameActivity.java"
    We implement Overrided method "onClick".
    After filling up EditText on click button
    user return the main activity.

--- simpleactivityresult/src/main/java/ru/startandroid/develop/simpleactivityresult/NameActivity.java
+++ simpleactivityresult/src/main/java/ru/startandroid/develop/simpleactivityresult/NameActivity.java
@@ -2,13 +2,32 @@ package ru.startandroid.develop.simpleactivityresult;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class NameActivity extends AppCompatActivity {
+public class NameActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etName;
+    Button btnOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.name);
+
+        etName = findViewById(R.id.etName);
+        btnOK = findViewById(R.id.btnOK);
+        btnOK.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.putExtra("name", etName.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
