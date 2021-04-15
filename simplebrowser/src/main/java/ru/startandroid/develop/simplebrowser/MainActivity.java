8c84e65e0c17361920b305f9a026f491718f828e
leil25 <nuriaxmetova93@mail.ru>
2021-04-15 14:46:13 +0300

    Create "SimpleBrowser" activity
    This activity is used to show example of creating browser.

---
+++ simplebrowser/src/main/java/ru/startandroid/develop/simplebrowser/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.startandroid.develop.simplebrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
