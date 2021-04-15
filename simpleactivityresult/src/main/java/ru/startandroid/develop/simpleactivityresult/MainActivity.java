d9b6a9f0a48ace351d778c4b675f99c540cb4ab5
leil25 <nuriaxmetova93@mail.ru>
2021-04-15 12:00:15 +0300

    Create methods "onClick" and "onActivityResult"
    We go to the Activity named NameActivity on click the button.
    We get result and show it in screen on activate method "onActivityResult".

--- simpleactivityresult/src/main/java/ru/startandroid/develop/simpleactivityresult/MainActivity.java
+++ simpleactivityresult/src/main/java/ru/startandroid/develop/simpleactivityresult/MainActivity.java
@@ -1,14 +1,39 @@
 package ru.startandroid.develop.simpleactivityresult;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvName;
+    Button btnName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvName = findViewById(R.id.tvName);
+        btnName = findViewById(R.id.btnName);
+        btnName.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, NameActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String name = data.getStringExtra("name");
+        tvName.setText("Your name is "+ name);
     }
 }
