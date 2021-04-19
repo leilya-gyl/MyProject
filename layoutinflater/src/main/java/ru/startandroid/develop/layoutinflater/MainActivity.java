5731cf446af9a8a1fbe85ba5df08150ecc1d4c0a
leil25 <nuriaxmetova93@mail.ru>
2021-04-19 08:01:54 +0300

    Create activity "LayoutInflater"
    This activity is used to see the usage of class "Inflater" and namely method "inflate".

---
+++ layoutinflater/src/main/java/ru/startandroid/develop/layoutinflater/MainActivity.java
@@ -0,0 +1,28 @@
+package ru.startandroid.develop.layoutinflater;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    final String LOG_TAG = "myLogs";
+
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        LayoutInflater ltInflater = getLayoutInflater();
+        View view = ltInflater.inflate(R.layout.text, null, false);
+        LayoutParams lp = view.getLayoutParams();
+
+        Log.d(LOG_TAG, "Class of view: " + view.getClass().toString());
+        Log.d(LOG_TAG, "LayoutParams of view is null: " + (lp == null));
+        Log.d(LOG_TAG, "Text of view: " + ((TextView) view).getText());
+    }
+}
