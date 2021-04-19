5fa1647d88247607ed77ec70f33663ffbaafd1e5
leil25 <nuriaxmetova93@mail.ru>
2021-04-19 08:31:57 +0300

    Add new view from second layout-file
    We get view from "text.xml"
    and add to "main.xml" 'linlayout' and 'rellayout'.

--- layoutinflater/src/main/java/ru/startandroid/develop/layoutinflater/MainActivity.java
+++ layoutinflater/src/main/java/ru/startandroid/develop/layoutinflater/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -19,14 +20,20 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
 
         LayoutInflater ltInflater = getLayoutInflater();
-        View view = ltInflater.inflate(R.layout.text, null, false);
-        LayoutParams lp = view.getLayoutParams();
-
         LinearLayout linLayout = findViewById(R.id.linLayout);
-        linLayout.addView(view);
+        View view1 = ltInflater.inflate(R.layout.text, linLayout, true);
+        LayoutParams lp1 = view1.getLayoutParams();
 
-        Log.d(LOG_TAG, "Class of view: " + view.getClass().toString());
-        Log.d(LOG_TAG, "LayoutParams of view is null: " + (lp == null));
-        Log.d(LOG_TAG, "Text of view: " + ((TextView) view).getText());
+        Log.d(LOG_TAG, "Class of view1: " + view1.getClass().toString());
+        Log.d(LOG_TAG, "Class of layoutParams of view1: " + lp1.getClass().toString());
+        //Log.d(LOG_TAG, "Text of view1: " + ((TextView) view1).getText());
+
+        RelativeLayout rellayout = findViewById(R.id.relLayout);
+        View view2 = ltInflater.inflate(R.layout.text, rellayout, true);
+        LayoutParams lp2 = view2.getLayoutParams();
+
+        Log.d(LOG_TAG, "Class of view2: " + view2.getClass().toString());
+        Log.d(LOG_TAG, "Class of layoutParams of view2: " + lp2.getClass().toString());
+        //Log.d(LOG_TAG, "Text of view2: " + ((TextView) view2).getText());
     }
 }
